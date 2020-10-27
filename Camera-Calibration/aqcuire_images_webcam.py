a90c3434bc692d5a6499cafdf873111431baf93b
damian.schori <damian.schori@gmail.com>
2020-10-27 15:41:22 +0100

    added webcam

---
+++ Camera-Calibration/aqcuire_images_webcam.py
@@ -0,0 +1,44 @@
+# MIT License
+# Copyright (c) 2019 JetsonHacks
+# See license
+# Using a CSI camera (such as the Raspberry Pi Version 2) connected to a
+# NVIDIA Jetson Nano Developer Kit using OpenCV
+# Drivers for the camera and OpenCV are included in the base image
+
+import cv2
+from time import sleep
+from pathlib import Path
+
+
+# gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
+# Defaults to 1280x720 @ 60fps
+# Flip the image by setting the flip_method (most common values: 0 and 2)
+# display_width and display_height determine the size of the window on the screen
+
+
+def grab_images(save_path):
+    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+    Path(save_path).mkdir(parents=True, exist_ok=True)
+    cap = cv2.VideoCapture(0)
+    if cap.isOpened():
+        window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
+        # Window
+        i = 0
+        while cv2.getWindowProperty("CSI Camera", 0) >= 0:
+            ret_val, img = cap.read()
+            cv2.imwrite('{}/image_{}.png'.format(save_path, i), img)
+            cv2.imshow("CSI Camera", img)
+            # This also acts as
+            keyCode = cv2.waitKey(30) & 0xFF
+            # Stop the program on the ESC key
+            if keyCode == 27:
+                break
+            i += 1
+            cv2.waitKey(1000)
+        cap.release()
+        cv2.destroyAllWindows()
+    else:
+        print("Unable to open camera")
+
+if __name__ == "__main__":
+    grab_images(save_path='raw_images')
